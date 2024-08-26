c156f185b1790e341c01df0fbbc121753a3c8b2e
Enolekoi <fc.severin@protonmail.com>
2024-08-26 10:03:32 +0200

    modified compareTimeDomain() to plot intensity and phase

--- visualize.py
+++ visualize.py
@@ -21,47 +21,47 @@ def save_plot_training_loss(loss_values, filepath):
     logger.info(f"Plot writen to {filepath}")
 
 '''
-Plot Training Loss
+compareTimeDomain()
+
+Compate Time Domains of label and prediction
+Inputs:
+    filepath    -> path to where the plot is saved
+    label       -> label of data
+    prediction  -> predicted data
+Outputs:
+    NULL
 '''
 def compareTimeDomain(filepath, label, prediction):
-
-    orig_real = label[:256]
-    orig_imag = label[256:]
-    pred_real = prediction[:256]
-    pred_imag = prediction[256:]
     
-    orig_abs = np.abs(orig_real + 1j* orig_imag) 
-    pred_abs = np.abs(pred_real + 1j* pred_imag)
+    orig_intensity = label[:256]
+    orig_phase = label[256:]
+    pred_intensity = prediction[:256]
+    pred_phase = prediction[256:]
+    
+    # orig_abs = np.abs(orig_real + 1j* orig_imag) 
+    # pred_abs = np.abs(pred_real + 1j* pred_imag)
 
-    fig, axs = plt.subplots(4,1, figsize=(8,14))
+    fig, axs = plt.subplots(3,1, figsize=(8,14))
     # Plotting the Intensity
-    axs[0].plot(orig_abs, label="original pulse", color="green")
-    axs[0].plot(pred_abs, label="predicted pulse", color="red")
+    axs[0].plot(orig_intensity, label="original pulse", color="green")
+    axs[0].plot(pred_intensity, label="predicted pulse", color="red")
     axs[0].set_title("Comparison of the intensities of the E-Field")
     axs[0].set_ylabel("Intensity of E-Field")
     axs[0].set_xlabel("Time in fs")
     axs[0].grid(True)
     axs[0].legend()
 
-    # Plotting the Real Part
-    axs[1].plot(orig_real, label="original pulse", color="green")
-    axs[1].plot(pred_real, label="predicted pulse", color="red")
-    axs[1].set_title("Comparison of the real parts of the E-Field")
-    axs[1].set_ylabel("real part of E-Field")
+    # Plotting the Phase
+    axs[1].plot(orig_phase, label="original pulse", color="green")
+    axs[1].plot(pred_phase, label="predicted pulse", color="red")
+    axs[1].set_title("Comparison of the phase of the E-Field")
+    axs[1].set_ylabel("Phase of E-Field")
     axs[1].set_xlabel("Time in fs")
     axs[1].grid(True)
     axs[1].legend()
 
-    axs[2].plot(orig_imag, label="original pulse", color="green")
-    axs[2].plot(pred_imag, label="predicted pulse", color="red")
-    axs[2].set_title("Comparison of the imaginary parts of the E-Field")
-    axs[2].set_ylabel("imaginary part of E-Field")
-    axs[2].set_xlabel("Time in fs")
-    axs[2].grid(True)
-    axs[2].legend()
-    
     # Plot Intensity difference
-    intensity_diff = orig_abs - pred_abs
+    intensity_diff = orig_intensity - pred_intensity
     axs[3].plot(intensity_diff, color="blue")
     axs[3].set_title("Intensity difference of the original and predicted pulse")
     axs[3].set_ylabel("Intensity difference of the original and predicted pulse")
