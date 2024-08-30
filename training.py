b5cf5249f8707bc36da96e3bbe5cf4b664314633
Enolekoi <fc.severin@protonmail.com>
2024-08-30 10:50:44 +0200

    added output size to label_reader constructor

--- training.py
+++ training.py
@@ -46,7 +46,7 @@ logger.info(f"Initial learning rate: {config.LEARNING_RATE}")
 
 # Transforms
 spec_transform = helper.ResampleSpectrogram(config.OUTPUT_NUM_DELAYS, config.OUTPUT_TIMESTEP, config.OUTPUT_NUM_WAVELENGTH, config.OUTPUT_START_WAVELENGTH, config.OUTPUT_END_WAVELENGTH)
-label_reader = helper.ReadLabelFromEs()
+label_reader = helper.ReadLabelFromEs(config.OUTPUT_SIZE)
 label_phase_correction = helper.RemoveAbsolutePhaseShift()
 label_scaler = helper.ScaleLabel(max_intensity=config.MAX_INTENSITY, max_phase=config.MAX_PHASE)
 label_transform = transforms.Compose([label_reader, label_phase_correction, label_scaler])
