bbf4a7bec5b14add4bda56d69c0c1c020c150eaf
Enolekoi <fc.severin@protonmail.com>
2024-09-02 10:25:52 +0200

    added extra channels

--- prediction.py
+++ prediction.py
@@ -66,8 +66,9 @@ def predict(spectrogram):
 logger.info(f"Loading spectrogram from {spectrogram_path}")
 orig_spec, orig_time, orig_wave, spec, out_time, out_wave = spec_transform(spectrogram_path)
 spec = torch.tensor(spec, dtype=torch.float64)
+spec = torch.unsqueeze(spec, dim=0)  # Add batch dimension [1, 512, 512]
 spec = torch.unsqueeze(spec, dim=0)  # Add batch dimension [1, 1, 512, 512]
-spec = torch.unsqueeze(spec, dim=0)  # Add batch dimension [1, 1, 512, 512]
+spec = spec.repeat(1, 3, 1, 1)  # Repeat the channel 3 times, changing [1, 1, 512, 512] to [1, 3, 512, 512] 
 
 label = label_transform(label_path)
 prediction = predict(spec)
