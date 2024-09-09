543263bc1e9d7c1512c91e487556d4d92d405874
Enolekoi <fc.severin@protonmail.com>
2024-09-09 12:03:25 +0200

    bugfix

--- helper.py
+++ helper.py
@@ -536,7 +536,7 @@ class ScaleLabel(object):
             scaled_label -> List of arrays containing intesity of time signal (squared amplitute) and it's phase
                 scaled to [-1,1] [tensor]
         '''
-        length_label = len(label)
+        length_label = label.size()
         half_size = int(length_label //2)
         intensity = label[:half_size]  # First half -> intensity
         phase = label[half_size:]      # Second half -> phase
