6bd8f3b6a14fc7bd6e0e4f5fa1d481c43c881fd3
Enolekoi <fc.severin@protonmail.com>
2024-09-09 12:12:04 +0200

    bugfix

--- helper.py
+++ helper.py
@@ -537,7 +537,8 @@ class ScaleLabel(object):
                 scaled to [-1,1] [tensor]
         '''
         print(type(label))
-        length_label = label
+        length_label = len(label)
+        print(lenght_label)
         half_size = int(length_label //2)
         intensity = label[:half_size]  # First half -> intensity
         phase = label[half_size:]      # Second half -> phase
