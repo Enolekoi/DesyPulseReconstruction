changed scaling and corrected datails about plot

training.py
@@ -129,7 +129,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=config.LEARNING_RATE)
 # optimizer = torch.optim.SGD(model.parameters(), lr=config.LEARNING_RATE, momentum=0.9)
 
 # scheduler for changing learning rate after each epoch
-scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.5)
+scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
 
 # list containing all loss values
 training_losses = []