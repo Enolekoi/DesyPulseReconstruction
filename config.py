2f132d468f7b7ff27fde8f5ab66e20d7d16cc922
Enolekoi <fc.severin@protonmail.com>
2024-09-16 13:03:41 +0200

    added incrementing prediciton plot file name

--- config.py
+++ config.py
@@ -31,6 +31,7 @@ ModelName = "trained_model_"
 ModelPath = "./models/trained_model_7.pth"
 LogName = "training_"
 TrainingLossPlotName = "training_loss_"
+PredicitonPlotName = "post_training_prediction_"
 
 # Path = "/mnt/data/desy/frog_simulated/grid_512_v2/"
 # Path = "/mnt/data/desy/frog_simulated/grid_256/"
@@ -52,12 +53,14 @@ Input:
     model_base_filename     -> filename of saved models without index and file ending
     log_base_filename       -> filename of logs without index and file ending
     loss_plot_base_filename -> filename of loss plots without index and file ending
+    prediction_base_filename-> filename of prediction plot without index and file ending
 Output:
     model_filepath      -> filepath of the next model file to be saved
     log_filepath        -> filepath of the next log file to be saved
     loss_plot_filepath  -> filepath of the next loss plot file to be saved
+    prediction_filepath -> filepath of the next prediction plot file to be saved
 '''
-def getFilepathIndex(model_directory, log_directory, model_base_filename, log_base_filename, loss_plot_base_filename):
+def getFilepathIndex(model_directory, log_directory, model_base_filename, log_base_filename, loss_plot_base_filename, prediction_base_filename):
 
     # Check if log and model directory exists, if not create them
     if not os.path.exists(model_directory):
@@ -72,7 +75,8 @@ def getFilepathIndex(model_directory, log_directory, model_base_filename, log_ba
     # Filter out matching file names and write them in a list
     matching_model_files = [f for f in model_files if f.startswith(model_base_filename) and f.endswith('.pth')]
     matching_loss_plot_files = [f for f in log_files if f.startswith(loss_plot_base_filename) and f.endswith('.png')]
-    matching_log_files= [f for f in log_files if f.startswith(log_base_filename) and f.endswith('.png')]
+    matching_log_files= [f for f in log_files if f.startswith(log_base_filename) and f.endswith('.log')]
+    matching_prediction_files = [f for f in log_files if f.startswith(prediction_base_filename) and f.endswith('.png')]
 
     # Determin what the next model index is
     if matching_model_files:
@@ -95,28 +99,38 @@ def getFilepathIndex(model_directory, log_directory, model_base_filename, log_ba
     else:
         next_loss_plot_index = 1
 
+    # Determin what the next prediction plot index is
+    if matching_prediction_files:
+        prediction_plot_numbers = [int(f[len(prediction_base_filename):-4]) for f in matching_prediction_files if f[len(prediction_base_filename):-4].isdigit()]
+        next_prediction_plot_index = max(prediction_plot_numbers) + 1 if prediction_plot_numbers else 1
+    else:
+        next_prediction_plot_index = 1
+
     # get the largest index
-    next_index = max(next_model_index, next_loss_plot_index, next_log_index)
+    next_index = max(next_model_index, next_loss_plot_index, next_log_index, next_prediction_plot_index)
 
     # Get the new filename
     new_model_filename = f"{model_base_filename}{next_index}.pth"
     new_loss_plot_filename = f"{loss_plot_base_filename}{next_index}.png"
     new_log_filename = f"{log_base_filename}{next_index}.log"
+    new_prediction_filename = f"{prediction_base_filename}{next_index}.png"
 
     # Join path and filenames together
     model_filepath = os.path.join(model_directory, new_model_filename)
     loss_plot_filepath = os.path.join(log_directory, new_loss_plot_filename)
     log_filepath = os.path.join(log_directory, new_log_filename)
+    prediction_filepath = os.path.join(log_directory, new_prediction_filename)
 
-    return model_filepath, log_filepath, loss_plot_filepath
+    return model_filepath, log_filepath, loss_plot_filepath, prediction_filepath
 
 ####################
 ## get file paths ##
 ####################
-model_filepath, log_filepath, loss_plot_filepath = getFilepathIndex(
+model_filepath, log_filepath, loss_plot_filepath, prediction_filepath = getFilepathIndex(
         model_directory=ModelDirectory,
         log_directory=LogDirectory,
         model_base_filename=ModelName,
         log_base_filename=LogName,
-        loss_plot_base_filename=TrainingLossPlotName
+        loss_plot_base_filename=TrainingLossPlotName,
+        prediction_base_filename=PredicitonPlotName
 )
