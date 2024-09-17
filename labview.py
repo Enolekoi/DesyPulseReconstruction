232afa786a6fdcad5a187ebe42dfd79cf108a1f5
Enolekoi <fc.severin@protonmail.com>
2024-09-17 13:17:39 +0200

    changed plot labview script

--- labview.py
+++ labview.py
@@ -1,23 +1,19 @@
 import matplotlib.pyplot as plt
 
 def plotSpectrogram(spectrogram):
-    print("1")
+    matplotlib.pyplot.ioff()    
     # Plot the spectrogram
     plt.figure(figsize=(10, 6))
-    print("2")
     # plt.imshow(spectrogram, aspect='auto', origin='lower', extent=[t.min(), t.max(), f.min(), f.max()], cmap='viridis')
     plt.imshow(spectrogram, aspect='auto', origin='lower', cmap='viridis')
 
-    print("3")
     # Add labels and title
     plt.colorbar(label='Intensity')  # or the appropriate unit for your data
     plt.ylabel("Wavelength [nm]")
     plt.xlabel("Time [fs]")
     plt.title("Spectrogram")
 
-    print("4")
     # Show the plot
     plt.show()
 
-    print("5")
     return True
