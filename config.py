e53b12599c71897081e2b6c7c9b53d6276044472
Enolekoi <fc.severin@protonmail.com>
2024-10-02 14:13:25 +0200

    fixed minor errors in loss function

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ import numpy as np
 import os
 import torch
 
-DESCRIPTOR = "Testing training grid_256_v3 - frog error as loss function , with 30 Epochs"
+DESCRIPTOR = "Testing training grid_256_v3 - regular error as loss function , with 100 Epochs"
 OUTPUT_SIZE = 256   # Amount of samples used for the reconstructed pulse [model output size should be 2*OUTPUT_SIZE]
 BATCH_SIZE = 10     # Amount of spectrograms trained at each step
-NUM_EPOCHS = 30     # Number of epochs to train the model
-UNFREEZE_EPOCH = 20 # Epoch after which the whole model is trained (before that only the output layers are trained)
+NUM_EPOCHS = 100     # Number of epochs to train the model
+UNFREEZE_EPOCH = 70 # Epoch after which the whole model is trained (before that only the output layers are trained)
 LEARNING_RATE = 1e-3    # Learning rate at the beginning of training
 WEIGHT_DECAY = 1e-5
 GAMMA_SCHEDULER = 0.9   # Learning rate de-/increases by this factor after each epoch
--- test.py
+++ test.py
@@ -23,13 +23,24 @@ N = 256
 
 
 # Initialize Resampler
-spec_transform = helper.ResampleSpectrogram(
+spec_reader = helper.ReadSpectrogram()
+spec_wave_transform = helper.ResampleSpectrogram(
     config.OUTPUT_NUM_DELAYS, 
     config.OUTPUT_TIMESTEP, 
     config.OUTPUT_NUM_FREQUENCIES,
     config.OUTPUT_START_FREQUENCY,
-    config.OUTPUT_END_FREQUENCY
+    config.OUTPUT_END_FREQUENCY,
+    type='wavelength'
     )
+spec_freq_transform = helper.ResampleSpectrogram(    
+    config.OUTPUT_NUM_DELAYS, 
+    config.OUTPUT_TIMESTEP, 
+    config.OUTPUT_NUM_FREQUENCIES,
+    config.OUTPUT_START_FREQUENCY,
+    config.OUTPUT_END_FREQUENCY,
+    type='frequency'
+    )
+
 # Initialize Label reader
 label_reader = helper.ReadLabelFromEs(config.OUTPUT_SIZE)
 label_ambig = helper.RemoveAmbiguitiesFromLabel(config.OUTPUT_SIZE)
@@ -48,36 +59,43 @@ label_analytical = torch.complex(label_real, label_imag)
 # calculate input wavelength axis
 Start = Center - (N/2)*Step      # calculate the first element of the wavelength input axis
 End = Center + (N/2)*Step       # calculate the last element of the wavelength input axis
-input_wave = np.linspace(Start, End, N)    # create array that corresponds tot the input wavelength axis
-input_freq = (2* torch.pi * SPEED_OF_LIGHT * 1e9) / input_wave # convert wavelenght [nm] to frequency [Hz]
-input_freq = input_freq[::-1]   # ensure increasing order of frequency
+input_wave = torch.linspace(Start, End, N)    # create array that corresponds tot the input wavelength axis
+input_freq = (2* torch.pi * SPEED_OF_LIGHT) / input_wave # convert wavelenght [nm] to frequency [Hz]
+
 # centerIdx = len(input_freq) // 2
 # wCenter = input_freq[centerIdx - 1]
-wCenter = (2* torch.pi * SPEED_OF_LIGHT * 1e9) / Center # convert wavelenght [nm] to frequency [Hz]
+wCenter = (2* torch.pi * SPEED_OF_LIGHT) / Center # convert wavelenght [nm] to frequency [Hz]
 
 tStart = -int(N / 2) * Ts    # calculate time at which the input time axis starts
 tEnd = tStart + (Ts * N) - Ts    # calculate the last element of the input time axis
-input_time = np.linspace(tStart, tEnd, N)   # create array that corresponds to the input time axis
+input_time = torch.linspace(tStart, tEnd, N)   # create array that corresponds to the input time axis
         
 # calculate SHG Matrix from analytical signal
 spec = loss.createSHGmat(label_analytical, Ts, wCenter)
+spec = torch.abs(spec)**2
 # resample SHG Matrix
-sim, input_time, input_freq, spec, spec_output_time, spec_output_freq = spec_transform.resampleFreq(spec, input_time, input_freq)
-print(f"owStart = {spec_output_freq.min():.4e}")
-print(f"owEnd = {spec_output_freq.max():.4e}")
-print(f"otStart = {spec_output_time.min():.4e}")
-print(f"otEnd = {spec_output_time.max():.4e}")
+spec_data = [spec, input_time, input_freq]
+sim, input_time, input_freq, spec, spec_output_time, spec_output_freq = spec_freq_transform(spec_data)
+# print(f"owStart = {spec_output_freq.min():.4e}")
+# print(f"owEnd = {spec_output_freq.max():.4e}")
+# print(f"otStart = {spec_output_time.min():.4e}")
+# print(f"otEnd = {spec_output_time.max():.4e}")
 
 # read and resample spectrogram from file
-sim_spec, sim_input_time, sim_input_wavelength, sim_output_spec, sim_output_time, sim_output_freq = spec_transform(PathSpec)
-print(f"min wavelenght: {sim_input_wavelength.min():.4e}")
-print(f"max wavelenght: {sim_input_wavelength.max():.4e}")
+spec_data_file = spec_reader(PathSpec)
+sim_spec, sim_input_time, sim_input_wavelength, sim_output_spec, sim_output_time, sim_output_freq = spec_wave_transform(spec_data_file)
 
+# get original spectrogram (without 3 identical channels)
+original_spectrogram = sim_output_spec
+print(f"og_spec min: {torch.min(original_spectrogram)}")
+print(f"og_spec max: {torch.max(original_spectrogram)}")
+
+loss.calcFrogError(original_spectrogram, spec)
 # Plot
 fig, axs = plt.subplots(2, figsize=(10,8))
 # Simuliertes Spektrogram (original)
 ax = axs[0]
-cax1 = ax.pcolormesh(sim_output_spec.numpy(), shading='auto')
+cax1 = ax.pcolormesh(original_spectrogram.numpy(), shading='auto')
 ax.set_title('Simuliertes Spektrum (original)')
 # ax.set_xlabel('Time [fs]')
 # ax.set_ylabel('Wavelength [nm]')
@@ -85,14 +103,13 @@ fig.colorbar(cax1, ax=ax)
 
 # Simuliertes Spektrogram (rekonstruiert)
 ax = axs[1]
-cax2 = ax.pcolormesh(np.abs(spec.numpy()), shading='auto')
+cax2 = ax.pcolormesh(spec.numpy(), shading='auto')
 ax.set_title('Simuliertes Spektrum (rekonstruiert)')
 # ax.set_xlabel('Time [fs]')
 # ax.set_ylabel('Frequency [Hz]')
 fig.colorbar(cax2, ax=ax)
 
-# Layout anpassen
-plt.tight_layout()
+# Layout anpassen plt.tight_layout()
 
 # Zeige die Plots an
-plt.show()
+# plt.show()
