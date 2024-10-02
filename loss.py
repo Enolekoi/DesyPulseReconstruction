643ad0827db59d2beaf7ffbe996434cb83d42b0e
Enolekoi <fc.severin@protonmail.com>
2024-10-02 12:18:05 +0200

    hopefully fixed device error

--- loss.py
+++ loss.py
@@ -177,13 +177,13 @@ def calcFrogError(Tref, Tmeas):
     # Ensure Tref and Tmeas are tensors, not tuples
     if isinstance(Tref, tuple):
         Tref = Tref[0]  # Extract the tensor if Tref is a tuple
-        print("Tref is a touple 1")
-        if isinstance(Tref, tuple):
-            Tref= Tref[0]
-            print("Tref is a touple 3")
     if isinstance(Tmeas, tuple):
         Tmeas = Tmeas[0]  # Extract the tensor if Tmeas is a tuple
-        print(2)
+
+    device = Tmeas.device
+    Tref.to(device)
+    Tmeas.to(device)
+
     mu = torch.sum(torch.matmul(Tmeas, Tref)) / torch.sum(torch.matmul(Tref, Tref)) # pypret gl. 13 (s. 497)
     r = torch.sum( (Tmeas - mu*Tref)**2)    # pypret gl. 11 (s. 497)
 
