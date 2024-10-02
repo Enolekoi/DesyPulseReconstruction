268ddde579acf038438a83a7af7ab6ec01845825
Enolekoi <fc.severin@protonmail.com>
2024-10-02 12:20:27 +0200

    hopefully fixed device error

--- loss.py
+++ loss.py
@@ -174,14 +174,14 @@ def createSHGmat(yta, Ts, wCenter):
     return shgMat
 
 def calcFrogError(Tref, Tmeas):
+    device = Tref.device
+    Tmeas.to(device)
+
     # Ensure Tref and Tmeas are tensors, not tuples
     if isinstance(Tref, tuple):
         Tref = Tref[0]  # Extract the tensor if Tref is a tuple
     if isinstance(Tmeas, tuple):
         Tmeas = Tmeas[0]  # Extract the tensor if Tmeas is a tuple
-
-    device = Tmeas.device
-    Tref.to(device)
 
     mu = torch.sum(torch.matmul(Tmeas, Tref)) / torch.sum(torch.matmul(Tref, Tref)) # pypret gl. 13 (s. 497)
     r = torch.sum( (Tmeas - mu*Tref)**2)    # pypret gl. 11 (s. 497)
