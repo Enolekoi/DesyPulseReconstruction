2503c7f22169f2022e18d43d733377fbff3ec520
Enolekoi <fc.severin@protonmail.com>
2024-10-25 11:31:20 +0200

    improved labview function

--- labview/labview.py
+++ labview/labview.py
@@ -4,26 +4,32 @@ labview.py Module
 Module containing functions to be integrated into the labview programm
 '''
 import matplotlib.pyplot as plt
+import torch
+
+from modules import constants as c
+from modules import preprocessing
 
 '''
 plotSpectrogram
 
 Plot a spectrogram using matplotlib
 '''
-def predict_time_domain(header, shg_matrix):
+def predictTimeDomain(header_string, shg_matrix):
     '''
     Inputs:
-        header          -> header of the spectrogram
+        header_string   -> header of the spectrogram as a string
         shg_matrix      -> Matrix containing spectrogram to be plotted 
     Outputs:
         finished        -> Variable containing information if function is finished [boolean]
     '''
-    finished = False
+    header = getHeaderFromString(header_string)
+    delay_axis, wavelength_axis = preprocessing.generateAxes(header)
+
     plt.ioff()    
     # Plot the spectrogram
     plt.figure(figsize=(10, 6))
     # plt.imshow(spectrogram, aspect='auto', origin='lower', extent=[t.min(), t.max(), f.min(), f.max()], cmap='viridis')
-    plt.imshow(shg_matrix, aspect='auto', origin='lower', cmap='viridis')
+    plt.imshow(delay_axis, wavelength_axis, shg_matrix.T, aspect='auto', origin='lower')
 
     # Add labels and title
     plt.colorbar(label='Intensity')  # or the appropriate unit for your data
@@ -37,3 +43,22 @@ def predict_time_domain(header, shg_matrix):
     finished = True
 
     return finished
+
+def getHeaderFromString(header_string):
+    # split the string into a list
+    header_list = header_string.split()
+    
+    # make the first two elements of the header_list integers and the last three floats
+    # delta_tau to fs
+    # delta_lambda, lambdaCenter to nm
+    header_list = [
+            int(header_list[0]),
+            int(header_list[1]),
+            float(header_list[2]) * c.femto, 
+            float(header_list[3]) * c.nano,
+            float(header_list[4]) * c.nano
+            ]
+    # convert the header_list to a tensor
+    header = torch.tensor(header_list)
+
+    return header
---
+++ modules/constants.py
@@ -0,0 +1,16 @@
+import torch
+
+c0 = 299792458
+c2pi = 2 * torch.pi * c0
+
+kilo    = 1e3
+mega    = 1e6
+giga    = 1e9
+terra   = 1e12
+peta    = 1e15
+
+milli   = 1e-3
+micro   = 1e-6
+nano    = 1e-9
+pico    = 1e-12
+femto   = 1e-15
