66f8f17584865231753e13933b617e53e6cfa1d3
Enolekoi <fc.severin@protonmail.com>
2024-12-03 09:57:31 +0100

    modified preprocessing

--- modules/config.py
+++ modules/config.py
@@ -54,9 +54,9 @@ Options that configure how the resampled SHG-matrixes are created
 OUTPUT_NUM_DELAYS = 512                         # Number of delays the SHG-matrix get resampled to
 OUTPUT_NUM_WAVELENGTH = 512                     # Number of delays the SHG-matrix get resampled to
 OUTPUT_NUM_FREQUENCIES = OUTPUT_NUM_WAVELENGTH  # Number of frequencies the SHG-matrix get resampled to
-OUTPUT_TIMESTEP = 59*c.femto                   # Size of timestep between delays [fs]
-OUTPUT_START_WAVELENGTH = 488*c.nano            # Smallest wavelength in the dataset [nm]
-OUTPUT_END_WAVELENGTH = 541*c.nano              # Largest wavelength in the dataset [nm]
+OUTPUT_TIMESTEP = 9.79*c.femto                   # Size of timestep between delays [fs]
+OUTPUT_START_WAVELENGTH = 489.85*c.nano            # Smallest wavelength in the dataset [nm]
+OUTPUT_END_WAVELENGTH = 540.04*c.nano              # Largest wavelength in the dataset [nm]
 OUTPUT_START_FREQUENCY = c.c2pi / OUTPUT_START_WAVELENGTH    # convert start wavelength to frequency [Hz]
 OUTPUT_END_FREQUENCY = c.c2pi / OUTPUT_END_WAVELENGTH        # convert stop wavelength to frequency [Hz]
 
