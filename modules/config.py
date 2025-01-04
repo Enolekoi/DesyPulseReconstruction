dcaadd18d885805c55ee5e2d518e263030e360a0
Enolekoi <fc.severin@protonmail.com>
2025-01-04 10:14:52 +0100

    full dataset 10 epochs

--- modules/config.py
+++ modules/config.py
@@ -25,7 +25,7 @@ MAX_LEARNING_RATE = 2.13e-6
 WEIGHT_DECAY = 1e-5     # TODO find description
 GAMMA_SCHEDULER = 0.9   # Learning rate de-/increases by this factor after each epoch, when using exponential LR decrease
 TBDRMS_THRESHOLD = 20   # Only data with a TBDrms higher than this threshold is used for training
-DESCRIPTOR = f"Training using new dataset (1/10) and no FROG-Error - with {NUM_EPOCHS} Epochs"
+DESCRIPTOR = f"Training using new dataset and no FROG-Error - with {NUM_EPOCHS} Epochs"
 
 '''
 Loss function options
