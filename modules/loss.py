1a530d0bfe4fd9c6cfb72afb9929fe7f4e7329c5
Enolekoi <fc.severin@protonmail.com>
2025-01-08 23:27:38 +0100

    unsupervised training

--- modules/loss.py
+++ modules/loss.py
@@ -202,6 +202,9 @@ class PulseRetrievalLossFunction(nn.Module):
                 mse_loss += (mse_real_mean + mse_imag_mean + mse_intensity_mean + mse_phase_mean) / self.mse_weight_sum
 
             # calculate weighted mean loss of mse loss and frog error
+            print(f"mse_loss = {mse_loss}")
+            print(f"frog_error = {frog_error}")
+            print(f"frog_error_weight = {self.frog_error_weight}")
             loss += mse_loss + frog_error*self.frog_error_weight
 
         # devide by batch size 
@@ -347,21 +350,21 @@ def calcFrogError(I_k, I_m):
     
     # calculate \mu [pypret gl. 13 (s. 497)]
     mu_numerator = torch.sum(I_m* I_k)
-    print(f"mu_numerator = {mu_numerator}")
+    # print(f"mu_numerator = {mu_numerator}")
     mu_denominator = torch.sum(I_k* I_k)
-    print(f"mu_denominator = {mu_denominator}")
+    # print(f"mu_denominator = {mu_denominator}")
     mu = mu_numerator / mu_denominator 
-    print(f"mu = {mu}")
+    # print(f"mu = {mu}")
     
     # calculate normalization factor
     norm_factor = 1 / (M * N)
-    print(f"norm_factor = {norm_factor}")
+    # print(f"norm_factor = {norm_factor}")
     # calculate the magnitude squared difference between the spectrograms
     mag_squared_difference = torch.abs(I_m - mu*I_k)**2
-    print(f"mag_squared_difference = {mag_squared_difference}")
+    # print(f"mag_squared_difference = {mag_squared_difference}")
     # calculate the FROG-Error
     frog_error = torch.sqrt(norm_factor * torch.sum(mag_squared_difference))
-    print(f"frog_error = {frog_error}")
+    # print(f"frog_error = {frog_error}")
 
     return frog_error
 
