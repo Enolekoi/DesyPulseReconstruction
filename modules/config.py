765cf19e4ec2456ed4a59cf469d3d4adda0465ac
Enolekoi <fc.severin@protonmail.com>
2025-01-10 11:24:40 +0100

    loading model first - Then unsupervised

--- modules/config.py
+++ modules/config.py
@@ -25,21 +25,21 @@ MAX_LEARNING_RATE = 0.03471
 WEIGHT_DECAY = 1e-5     # TODO find description
 GAMMA_SCHEDULER = 0.9   # Learning rate de-/increases by this factor after each epoch, when using exponential LR decrease
 TBDRMS_THRESHOLD = 20   # Only data with a TBDrms higher than this threshold is used for training
-DESCRIPTOR = f"Training supervised with variing hyperparameters, new dataset and no FROG-Error - with {NUM_EPOCHS} Epochs"
+DESCRIPTOR = f"Training unsupervised after loading model, new dataset and FROG-Error - with {NUM_EPOCHS} Epochs"
 
 '''
 Loss function options
 
 Options that configure how the loss function is used
 '''
-USE_LABEL = True
+USE_LABEL = False
 PULSE_THRESHOLD = 1e-3      # The Pulse is considered to be between the first and last value over the threshold
-PENALTY_FACTOR = 1.0        # Values outside the pulse are surpressed, by weighing their error with this factor
-WEIGTH_REAL_PART = 1.0      # Weight used for MSE of the real part
-WEIGTH_IMAG_PART = 1.0      # Weight used for MSE of the imaginary part
-WEIGTH_INTENSITY = 1.0      # Weight used for MSE of the intensity
+PENALTY_FACTOR = 0.0        # Values outside the pulse are surpressed, by weighing their error with this factor
+WEIGTH_REAL_PART = 0.0      # Weight used for MSE of the real part
+WEIGTH_IMAG_PART = 0.0      # Weight used for MSE of the imaginary part
+WEIGTH_INTENSITY = 0.0      # Weight used for MSE of the intensity
 WEIGTH_PHASE = 0.0          # Weight used for MSE of the phase (only considered, when there is a pulse)
-WEIGTH_FROG_ERROR = 0.0     # Weight used for the FROG Error (if it is 0.0, the calculation is skipped)
+WEIGTH_FROG_ERROR = 1.0     # Weight used for the FROG Error (if it is 0.0, the calculation is skipped)
 
 '''
 Scaling Options
