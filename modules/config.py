e3f467d39d53cd4e24b099b718e2c98d37ccb4c8
Enolekoi <fc.severin@protonmail.com>
2025-01-15 16:42:51 +0100

    changed hyperparameters

--- modules/config.py
+++ modules/config.py
@@ -25,7 +25,7 @@ MAX_LEARNING_RATE = 2.13e-05
 WEIGHT_DECAY = 1e-5     # TODO find description
 GAMMA_SCHEDULER = 0.9   # Learning rate de-/increases by this factor after each epoch, when using exponential LR decrease
 TBDRMS_THRESHOLD = 1.063   # Only data with a TBDrms higher than this threshold is used for training
-DESCRIPTOR = f"Training supervised 50% TBD, new dataset and no FROG-Error - with {NUM_EPOCHS} Epochs"
+DESCRIPTOR = f"Training supervised 90% TBD, new dataset and no FROG-Error - with {NUM_EPOCHS} Epochs"
 
 '''
 Loss function options
