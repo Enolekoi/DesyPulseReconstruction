94999f4c65b5ce5f65b5b3840a9251d5c022315c
Enolekoi <fc.severin@protonmail.com>
2025-01-15 14:15:32 +0100

    added logs

--- find_lr.py
+++ find_lr.py
@@ -168,7 +168,7 @@ model = models.CustomDenseNetReconstruction(
     num_outputs=config.OUTPUT_SIZE
     )
 # Define location of pretrained weights
-model.load_state_dict(torch.load('./logs/log_26/model.pth', weights_only=True))
+model.load_state_dict(torch.load('./logs/log_4/model.pth', weights_only=True))
 
 # set the model to float, send it to the selected device and put it in evaluation mode
 model.float().to(device).eval()
@@ -210,7 +210,7 @@ logger.info(f"Size of dataset: {length_dataset}")
 
 # get ratios of train, validation and test data
 test_size = 0                       # amount of test data (10%)
-validation_size = 90000                # amount of validation data (10%) 
+validation_size = 60000                # amount of validation data (10%) 
 train_size = length_dataset - test_size - validation_size   # amount of training and validation data (80%)
 logger.info(f"Size of training data:   {train_size}")
 
